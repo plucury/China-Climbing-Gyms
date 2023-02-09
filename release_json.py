15fab899463afdac80a2c914f321a3fdbfa0c6ce
plucury <plucury@gmail.com>
2023-02-09 21:51:27 +0800

    update release script & fix typo

--- release_json.py
+++ release_json.py
@@ -10,5 +10,18 @@ if __name__ == '__main__':
             with open(os.path.join(path, file), 'r') as f:
                 r = yaml.safe_load(f)
                 gyms.append(r)
+    city_dict = {}
+    last_city = None
+    for gym in gyms:
+        city = gym['city']
+        if city not in city_dict:
+            city_dict[city] = {}
+            city_dict[city]['gyms'] = []
+            city_dict[city]['city'] = city
+            city_dict[city]['province'] = gym['province']
+        city_dict[city]['gyms'].append(gym)
+    citys = sorted(city_dict.values(),
+                   key=lambda x: x['province']+":"+x['city'])
+
     with open('gyms.json', 'w') as f:
-        json.dump(gyms, f, indent=4)
+        json.dump(citys, f, indent=4)
